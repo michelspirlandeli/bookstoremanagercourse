82ba302ed2246132df2bfca7ac27854770067fb3
michelspirlandeli <michelspirlandeli10@gmail.com>
2020-12-27 10:09:46 -0300

    created base authors structure classes

---
+++ src/main/java/com/michelspirlandeli/bookstoremanager/author/mapper/AuthorMapper.java
@@ -0,0 +1,16 @@
+package com.michelspirlandeli.bookstoremanager.author.mapper;
+
+import com.michelspirlandeli.bookstoremanager.author.dto.AuthorDTO;
+import com.michelspirlandeli.bookstoremanager.author.entity.Author;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface AuthorMapper {
+
+    AuthorMapper INSTANCE = Mappers.getMapper(AuthorMapper.class);
+
+    Author toModel(AuthorDTO authorDTO);
+
+    AuthorDTO toDTO(Author author);
+}
